a9028fb9968a6e7cba00ee2895738969c88ee8b9
dannhu283 <dannhu2803@gmail.com>
2023-08-27 22:28:33 +0700

    add Dom

---
+++ dom/main.js
@@ -0,0 +1,59 @@
+const h1DOM = document.createElement("h1");
+
+h1DOM.title = "Hello";
+h1DOM.className = "heading";
+
+h1DOM.innerText = "Hello guys";
+
+document.body.appendChild(h1DOM);
+
+//react
+
+//React.createElement(type,props,chidlen,n)
+
+const h1React = React.createElement(
+  "h1",
+  {
+    title: "Hello",
+    className: "heading",
+  },
+  "hello guys"
+);
+
+//bt2
+
+const ulDOM = document.createElement("ul");
+ulDOM.id = "ul-id";
+ulDOM.style = "color:red;font-size:100px";
+
+const liDOM1 = document.createElement("li");
+liDOM1.innerText = "javascript";
+
+const liDOM2 = document.createElement("li");
+liDOM2.innerText = "Reacjs";
+
+ulDOM.appendChild(liDOM1);
+ulDOM.appendChild(liDOM2);
+
+document.body.appendChild(ulDOM);
+
+//react
+const ulReact = React.createElement(
+  "ul",
+  null,
+  React.createElement("li", null, "javascript"),
+  React.createElement("li", null, "React")
+);
+
+console.log(ulReact);
+
+//bt3
+
+const divReact = React.createElement(
+  "div",
+  { class: "post-item" },
+  React.createElement("h2", { title: "học React tại F8" }, "học ReactJs"),
+  React.createElement("p", null, "Học react từ cơ bản đến nâng cao")
+);
+
+console.log(divReact);
